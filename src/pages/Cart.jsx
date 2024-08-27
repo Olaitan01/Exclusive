aa583e6a06100b9b306d30726a3b9342c48b8e39
unknown <abiodunfatunbi4@gmail.com>
2024-08-28 00:12:34 +0100

    fix: imageMap

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,23 +6,8 @@ import ViewAllBtn from "../components/ViewAllBtn";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
 
-import dogsFood from "../assets/images/Frame 604 (1).png";
-import camera from "../assets/images/Frame 604.png";
-import laptop from "../assets/images/ideapad-gaming-3i-01-500x500 1.png";
-import curology from "../assets/images/curology-j7pKVQrTUsM-unsplash 1.png";
-import kidsCar from "../assets/images/Frame 608.png";
-import jacket from "../assets/images/Frame 608 (3).png";
-import boots from "../assets/images/Frame 608 (1).png";
-import pads from "../assets/images/Frame 608 (2).png";
-import GucciBag from "../assets/images/547953_9C2ST_8746_001_082_0000_Light-Gucci-Savoy-medium-duffle-bag 1.png";
-import RainCoat from "../assets/images/Frame 605.png";
-import CPUCooler from "../assets/images/Frame 610.png";
-import BookShelf from "../assets/images/Frame 612.png";
-import GamingLaptop from "../assets/images/ideapad-gaming-3i-01-500x500 1.png";
-import Gamepad from "../assets/images/Frame 611.png";
-import keyboard from "../assets/images/ak-900-01-500x500 1.png";
-import GamingMonitor from "../assets/images/g27cq4-500x500 1.png";
-import ComfortChair from "../assets/images/sam-moghadam-khamseh-kvmdsTrGOBM-unsplash 1.png";
+import imageMap from "../components/ImageMap";
+
 import {
   addToCart,
   decreamentCartItem,
@@ -32,27 +17,6 @@ import {
 import { useEffect } from "react";
 
 function Cart() {
-  const imageMap = {
-    "Dog Food": dogsFood,
-    "CANON Camera": camera,
-    "Asus Laptop": laptop,
-    "Curology product": curology,
-    "Kids Car": kidsCar,
-    Boots: boots,
-    pad: pads,
-    Jacket: jacket,
-    "Rain Coat": RainCoat,
-    "Gucci Bag": GucciBag,
-    "CPU Cooler": CPUCooler,
-    "Small BookShelf": BookShelf,
-    "Gaming laptop": GamingLaptop,
-    Gamepad: Gamepad,
-    keyboard: keyboard,
-    "Gaming-monitor": GamingMonitor,
-    "Comfort chair": ComfortChair,
-    "Gucci bag": GucciBag,
-  };
-
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
@@ -168,7 +132,7 @@ function Cart() {
                     placeholder="Coupon code"
                     className="border-2 border-primaryBlk bg-inherit rounded-sm p-2 text-sm placeholder:text-gray-400 placeholder:text-[1rem] font-light w-[100%]  desktop:w-60 tablet:w-60 "
                   />
-                  <ViewAllBtn buttonText="Apply Coupon"  />
+                  <ViewAllBtn buttonText="Apply Coupon" />
                 </div>
                 <div className="border-2 border-primaryBlk rounded-md w-full  desktop:w-80 p-4">
                   <h3 className="font-medium py-2">Cart Total</h3>
